eb7a4bae6660347f4ac94e0ccfc8070115ef3f7f
ZX夏夜之风 <snwcreations@qq.com>
2025-04-24 14:00:28 +0800

    fix: log when registering payloads

--- fabric/src/main/java/snw/mods/ctweaks/mod/client/net/fabric/ClientNetHandlerImpl.java
+++ fabric/src/main/java/snw/mods/ctweaks/mod/client/net/fabric/ClientNetHandlerImpl.java
@@ -1,16 +1,19 @@
 package snw.mods.ctweaks.mod.client.net.fabric;
 
+import lombok.extern.slf4j.Slf4j;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import snw.mods.ctweaks.mod.client.net.ModPayload;
 
 import static snw.mods.ctweaks.mod.client.net.ModC2SConnection.getModC2SConnection;
 
+@Slf4j
 public final class ClientNetHandlerImpl {
     private ClientNetHandlerImpl() {
     }
 
     public static void registerPayloads() {
+        log.info("Registering payloads");
         PayloadTypeRegistry.playS2C().register(ModPayload.TYPE, ModPayload.CODEC);
         PayloadTypeRegistry.playC2S().register(ModPayload.TYPE, ModPayload.CODEC);
         ServerPlayNetworking.registerGlobalReceiver(ModPayload.TYPE, (payload, ctx) -> {
