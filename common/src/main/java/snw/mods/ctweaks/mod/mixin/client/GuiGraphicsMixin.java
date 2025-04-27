4c61d9b332a0598bc29d0b85ae7dd5fb6c78f4ec
ZX夏夜之风 <snwcreations@qq.com>
2025-04-28 07:32:24 +0800

    fix: use Minecraft instance stored in GuiGraphics object

--- common/src/main/java/snw/mods/ctweaks/mod/mixin/client/GuiGraphicsMixin.java
+++ common/src/main/java/snw/mods/ctweaks/mod/mixin/client/GuiGraphicsMixin.java
@@ -2,13 +2,17 @@ package snw.mods.ctweaks.mod.mixin.client;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 
 @SuppressWarnings("AddedMixinMembersNamePattern")
 @Mixin(GuiGraphics.class)
 public abstract class GuiGraphicsMixin implements snw.mods.ctweaks.mod.client.render.GuiGraphics {
+    @Shadow @Final private Minecraft minecraft;
+
     @Override
     public Minecraft getMinecraft() {
-        return Minecraft.getInstance();
+        return minecraft;
     }
 }
