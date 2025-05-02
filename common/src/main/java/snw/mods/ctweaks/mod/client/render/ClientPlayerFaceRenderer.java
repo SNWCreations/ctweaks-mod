b385f1691bb80dbf54a5085168d1e7d31d497930
ZX夏夜之风 <snwcreations@qq.com>
2025-05-02 11:51:28 +0800

    fix: should actively load skin if we don't have it

--- common/src/main/java/snw/mods/ctweaks/mod/client/render/ClientPlayerFaceRenderer.java
+++ common/src/main/java/snw/mods/ctweaks/mod/client/render/ClientPlayerFaceRenderer.java
@@ -1,5 +1,6 @@
 package snw.mods.ctweaks.mod.client.render;
 
+import com.mojang.authlib.GameProfile;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.PlayerInfo;
 import net.minecraft.client.resources.DefaultPlayerSkin;
@@ -13,6 +14,7 @@ import java.util.UUID;
 public class ClientPlayerFaceRenderer implements ClientRenderer {
     private final int id;
     private UUID targetId;
+    private boolean requesting;
     private PlanePosition position;
     private int size = 24;
     private ResourceLocation cachedSkinLocation;
@@ -35,17 +37,34 @@ public class ClientPlayerFaceRenderer implements ClientRenderer {
             }
         }
         if (cachedSkinLocation == null) {
+            if (!requesting) {
+                final UUID toRequest = targetId;
+                GameProfile profile = new GameProfile(toRequest, "");
+                minecraft.getSkinManager().getOrLoad(profile)
+                        .thenAccept(loaded -> {
+                            if (targetId.equals(toRequest)) {
+                                cachedSkinLocation = loaded.texture();
+                            } // or target changed so this texture should be dropped
+                        })
+                        .thenRun(() -> requesting = false);
+                requesting = true;
+            }
             // If server adds a player while the client have no information about it,
             //  we'll use default skin as fallback. The correct skin will be used when
             //  the info of the target player is received on the client through
-            //  player info update packet.
+            //  player info update packet or client loaded the textures.
             cachedSkinLocation = DefaultPlayerSkin.getDefaultTexture();
         }
         helper.drawPlayerFace(cachedSkinLocation, position.x(), position.y(), size);
     }
 
     public void update(ClientboundUpdatePlayerFaceRendererPacket packet) {
-        this.targetId = Optional.ofNullable(packet.getTarget()).orElse(this.targetId);
+        UUID newTarget = Optional.ofNullable(packet.getTarget()).orElse(this.targetId);
+        if (!newTarget.equals(this.targetId)) {
+            this.requesting = false;
+            this.cachedSkinLocation = null;
+        }
+        this.targetId = newTarget;
         this.position = Optional.ofNullable(packet.getPosition()).orElse(this.position);
         this.size = Optional.ofNullable(packet.getSize()).orElse(this.size);
     }
